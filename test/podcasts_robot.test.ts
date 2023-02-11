4eedea2851a791979627f015fdb870111b3d0c06
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-02-12 00:34:11 +0100

    Initalize project with AWS CDK (#1)

---
+++ test/podcasts_robot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PodcastsRobot from '../lib/podcasts_robot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/podcasts_robot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PodcastsRobot.PodcastsRobotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
