ea7ed1839cc542f47008f6196aaf405d4d62901b
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-02-14 10:45:11 +0100

    Remove unused files

--- lambda/src/stringFromStream.js
+++
@@ -1,5 +0,0 @@
-const bufferFromStream = require('./bufferFromStream.js');
-
-const stringFromStream = async (stream) => (await bufferFromStream(stream)).toString('utf8');
-
-module.exports = stringFromStream;
