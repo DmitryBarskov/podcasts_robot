f8af3065d771851a0a54d4200357c9cc1eac1df6
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-02-20 00:59:42 +0100

    Separate responsibilities between multiple lambdas (#10)
    * Separate responsibilities between multiple lambdas
    
    * Add js files

--- lambda/src/lambdaResponse.js
+++
@@ -1,9 +0,0 @@
-const lambdaResponse = (body) => ({
-  statusCode: 200,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(body),
-});
-
-module.exports = lambdaResponse;
---
+++ lambda/telegram-api/handler.js
@@ -0,0 +1,53 @@
+const telegramMethod = require("./telegramMethod.js");
+const randomEmoji = require('./randomEmoji.js');
+
+const processRecord = async (event) => {
+  if (event.type == 'invalidUrl') {
+    return await telegramMethod('sendMessage', {
+      body: {
+        chat_id: event.chatId,
+        reply_to_message_id: event.requestMessageId,
+        text: `I'm a robot! Send me a youtube video! ${randomEmoji()}`
+      },
+    });
+  } else if (event.type == 'downloadSuccess') {
+    const sendTitle = telegramMethod('sendMessage', {
+      body: {
+        chat_id: event.chatId,
+        reply_to_message_id: event.requestMessageId,
+        text: event.title,
+      },
+    });
+    const sendAudioChunks = event.audioData.map(chunk => {
+      return telegramMethod('sendAudio', {
+        body: {
+          chat_id: event.chatId,
+          reply_to_message_id: event.requestMessageId,
+          ...chunk,
+        },
+      });
+    });
+    return await Promise.all([sendTitle, ...sendAudioChunks]);
+  } else {
+    console.error('Unsupported event!', event);
+    return {};
+  }
+};
+
+/**
+ * Trigger from SQS schema:
+ *
+ * @param {object} event of type { Records: [ { body } ] },
+ * where body is a JSON string. JSON.parse(body) = {
+ *    type: 'downloadFailure' | 'downloadSuccess',
+ *    chatId, requestMessageId, audioData?
+ * }
+ */
+exports.handler = async (event) => {
+  let promises = event.Records.map(record => {
+    console.debug("Processing rec:", record);
+    return processRecord(JSON.parse(record.body));
+  });
+  await Promise.all(promises);
+  return {};
+};
